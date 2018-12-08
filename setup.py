7eaf8e5773c9beda1aab31981c4bf2864cb3c357
Tomáš Heger <heger.tomas@gmail.com>
2018-12-08 23:52:38 +0100

    Try including README as long_description (#10)

--- setup.py
+++ setup.py
@@ -8,53 +8,20 @@
 
 from setuptools import setup
 
-long_desc = """
-
-TwN Solitaire is a simple game inspired by 2048 Solitaire (but more
-generic) which is inspired by 2048 (but instead of tiles in a grid you collect
-cards here). I find the game fun and satisfying and thus I decided to try and
-implement it using Pygame. Here we are.
-
-The goal of the game is to stack an endless row of cards into limited-size
-columns. Each time you stack two cards of the same value one on the other,
-the two cards are replaced by one of double the value and your score grows.
-Cards with the maximum value (configurable, see below) will disappear.
-The game ends once all the columns are full and so no more cards can be played.
-
-
-CONTROLS
-
-At the top you can see your current score, below that is the main board where
-the cards are stacked. In the bottom part you can see two next cards that will
-come to the game. You can use 1, 2, 3 and 4 keys to place the next upcoming
-card to the respective column.
-
-
-CONFIGURATION
-
-There are some configurable options - see the default config file
-(.2-n_solitaire.conf). All options should be described there including default
-values which will be used if not specified otherwise. The game will search for
-the configuration file in the following locations (in this order):\n
--   current working directory\n
--   home directory\n
--   directory specified by TWN_SOLITAIRE_CONF_DIR environment variable
-
-
-Please report issues/ideas at Github.
-
-"""
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name='twn-solitaire',
-    version='0.0.1a3',
+    version='0.0.1a4',
     packages=['twnsol', ],
     scripts=['solitaire.py', ],
     url='https://github.com/geckon/2-n_solitaire',
     license='GNU GPL v3',
     description='Generic version of 2048 Solitaire. Simple, fun and '
                 'satisfying game.',
-    long_description=long_desc,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     install_requires=[
         "pygame == 1.9.4",
         "toml == 0.10.0",
